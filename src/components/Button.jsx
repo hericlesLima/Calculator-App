4cbc0c4cf90e3329192e82977da5af7086754355
hericlesLima <hericlesrecheado@gmail.com>
2021-08-16 09:54:19 -0100

    calculator commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import "./Button.css";
+
+export default (props) => {
+  let classes = "button";
+  classes += props.operation ? " operation" : "";
+  classes += props.double ? " double" : "";
+  classes += props.triple ? " triple" : "";
+
+  useEffect(() => {
+      console.log(classes)
+  })
+
+  return (
+    <button
+      onClick={(e) => props.click && props.click(props.label)}
+      className={classes}
+    >
+      {props.label}
+    </button>
+  );
+};
